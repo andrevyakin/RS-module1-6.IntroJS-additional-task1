c417dd7a981f58e6569bbca4588be157892eea48
Андрей <andrevyakin@gmail.com>
2022-11-09 17:42:20 +0300

    Is the first character null?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let javaScriptDescription = "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили. Является реализацией спецификации ECMAScript. JavaScript обычно используется как встраиваемый язык для программного доступа к объектам приложений.";
 
 //Обрежь строку наполовину с 1-го символа по символ, который находится посередине строки
-javaScriptDescription = javaScriptDescription.substring(1, Math.floor(javaScriptDescription.length / 2));
+javaScriptDescription = javaScriptDescription.substring(0, Math.floor(javaScriptDescription.length / 2));
 
 //В обрезанной строке необходимо заменить все строчные буквы “а” на прописные “А” (и латиницу, и кириллицу).
 javaScriptDescription = javaScriptDescription.replace(/[aа]/g, "A");
